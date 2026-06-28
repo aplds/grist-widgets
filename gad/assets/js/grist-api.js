5f36941bcc893cb073e162f62ebee235d6db97ef
aplds <pillods.arno@icloud.com>
2026-06-28 17:35:10 +0200

    Update grist-api.js

--- gad/assets/js/grist-api.js
+++ gad/assets/js/grist-api.js
@@ -1,21 +1,47 @@
 // Initialisation de l'API Grist
 grist.ready({ requiredAccess: 'read table' });
 
-// Récupération des données depuis Grist
+// Données des tables
 let affairesData = [];
-let audiencesData = [];
+let partiesData = [];
 let membresData = [];
-let modelesData = [];
+let audiencesData = [];
+let retroplanningData = [];
+let rolesData = [];
+let ongletsData = [];
+let observateursData = [];
+let ecolesData = [];
 
+// Écouteur pour les enregistrements des tables
 grist.onRecords(function(table) {
-    if (table.name === "Affaires") {
-        affairesData = table.records;
-    } else if (table.name === "Audiences") {
-        audiencesData = table.records;
-    } else if (table.name === "Membres") {
-        membresData = table.records;
-    } else if (table.name === "Modeles") {
-        modelesData = table.records;
+    switch (table.name) {
+        case "Affaires":
+            affairesData = table.records;
+            break;
+        case "Parties":
+            partiesData = table.records;
+            break;
+        case "Membres":
+            membresData = table.records;
+            break;
+        case "Audiences":
+            audiencesData = table.records;
+            break;
+        case "Retroplanning":
+            retroplanningData = table.records;
+            break;
+        case "Role":
+            rolesData = table.records;
+            break;
+        case "Onglets":
+            ongletsData = table.records;
+            break;
+        case "Observateurs":
+            observateursData = table.records;
+            break;
+        case "Ecoles":
+            ecolesData = table.records;
+            break;
     }
 });
 
@@ -24,9 +50,20 @@ function getAffaireById(id) {
     return affairesData.find(affaire => affaire.id === id);
 }
 
-// Fonction pour mettre à jour une affaire
-function updateAffaire(id, updates) {
-    grist.docApi.applyUserActions([
-        ["UpdateRecord", id, updates]
-    ]);
+// Fonction pour récupérer les parties d'une affaire
+function getPartiesByAffaireId(affaireId) {
+    return partiesData.filter(partie => partie.Affaire === affaireId);
+}
+
+// Fonction pour récupérer les membres d'une affaire
+function getMembresByAffaireId(affaireId) {
+    const affaire = getAffaireById(affaireId);
+    if (!affaire || !affaire.Membres_convoques) return [];
+    const membresIds = affaire.Membres_convoques;
+    return membresData.filter(membre => membresIds.includes(membre.id));
+}
+
+// Fonction pour récupérer les audiences d'une affaire
+function getAudiencesByAffaireId(affaireId) {
+    return audiencesData.filter(audience => audience.Affaire === affaireId);
 }
